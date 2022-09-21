f94ce46ffb9d025254913891a4a9b24799e6383d
Jorge Kimura <kimurajorge2001@gmail.com>
2022-09-20 22:27:29 -0300

    feat:estruturacao base

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ app.use(express.json());
     
 const PORT = process.env.PORT || 3000   
 
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`)
 });
